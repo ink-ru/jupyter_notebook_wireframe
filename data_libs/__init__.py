8bac0091423f6b6f93cdb84a4a5bb811fdd8abbf
Васин Юрий <george.a.wise@gmail.com>
2019-10-21 09:45:17 +0300

    Create __init__.py

---
+++ data_libs/__init__.py
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/1944569/how-do-i-write-good-correct-package-init-py-files
+
+__all__ = ["package_module1", "package_module2", "package_module3"]
+
+from data_libs.package_module1 package_module1_module1
+from data_libs.package_module2 package_module1_module1
